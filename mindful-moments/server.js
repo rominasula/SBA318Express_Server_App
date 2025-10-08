1da8a6e3ae6131aad6d505da4f93f2631969ef5d
rominasula <romina_sula@hotmail.com>
2025-10-07 21:55:02 -0400

    addMoment

--- mindful-moments/server.js
+++ mindful-moments/server.js
@@ -5,9 +5,21 @@ const logger = require("./middleware/logger.js");
 const errorHandler = require("./middleware/errorHandler.js");
 const momentsRouter = require("./routes/moments.js");
 
+app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+
+
+// Custom middleware
+app.use(logger);
+
+// Routes
+app.use("/moments", momentsRouter);
+
 // Home redirect
 app.get("/", (req, res) => res.redirect("/moments.js"));
 
+// Error handler
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Mindful Moments running on port ${port}.`);
